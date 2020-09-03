9b5c8f3a4544bc8994cfe958f7791396c9623105
Goothy <Sunil.Goothy@tmeic.in>
2020-09-03 18:08:44 +0530

    Added a valid date check function for momentum scanner

--- QuantTest/yfinance_test.py
+++ QuantTest/yfinance_test.py
@@ -1,5 +1,5 @@
 import yfinance as yf
 
-data = yf.download("AARTIDRUGS.NS RELIANCE.NS", start="2020-08-15", end="2020-08-23")
+data = yf.download("AARTIDRUGS.NS RELIANCE.NS", start="2019-09-04", end="2019-09-05")
 data = data.transpose()
 print(data)
